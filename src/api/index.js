8911461fe55a4362ab7d2f76b63e51e7d59b4151
rendyfrasya <rendyfrasya456@gmail.com>
2023-08-20 02:26:37 +0700

    Third Commit of FE_TA

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://lilserv666.andresakti.xyz:3000/api/main",
+  baseURL: "http://monsys.cloud:3000/api/main",
 });
 
 export default api;
